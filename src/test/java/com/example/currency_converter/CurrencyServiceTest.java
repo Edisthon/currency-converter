5eaef6b1b05fb6b75e666869523d9e75b3c2cc7c
Edisthon <edisthonshuti14@gmail.com>
2026-07-03 22:40:41 +0200

    Add the rest of the test logic

--- src/test/java/com/example/currency_converter/CurrencyServiceTest.java
+++ src/test/java/com/example/currency_converter/CurrencyServiceTest.java
@@ -1,11 +1,19 @@
 package com.example.currency_converter;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import com.example.currency_converter.model.ExchangeRate;
 import com.example.currency_converter.repository.CurrencyRepository;
 import com.example.currency_converter.repository.ExchangeRateRepository;
 import com.example.currency_converter.service.CurrencyService;
@@ -34,7 +42,17 @@ public class CurrencyServiceTest {
         String to = "RWF";
         double amount = 500;
         double rate = 0.92345;
+
+        ExchangeRate mockRate = new ExchangeRate("1", from, to, rate);
         
+        when(exchangeRateRepository.findByFromCurrencyAndToCurrency(from, to)).thenReturn(Optional.of(mockRate));
+
+
+        double result = currencyService.convert(from, to, amount);
+
+        assertEquals(9.235, result);
+        verify(exchangeRateRepository, times(1)).findByFromCurrencyAndToCurrency(from, to);
+
     }
     
 
