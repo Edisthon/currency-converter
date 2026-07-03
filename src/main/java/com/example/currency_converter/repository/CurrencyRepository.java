8407a38d4db155761bed980a51583d56d4526a39
Edisthon <edisthonshuti14@gmail.com>
2026-07-03 15:47:16 +0200

    Add the method we will be using to search by code

--- src/main/java/com/example/currency_converter/repository/CurrencyRepository.java
+++ src/main/java/com/example/currency_converter/repository/CurrencyRepository.java
@@ -1,9 +1,16 @@
 package com.example.currency_converter.repository;
 
+import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface CurrencyRepository {
+import com.example.currency_converter.model.Currency;
 
+@Repository
+public interface CurrencyRepository extends JpaRepository<Currency, String> {
+
+    Optional<Currency> findByCode(String code);
 
     
 }
