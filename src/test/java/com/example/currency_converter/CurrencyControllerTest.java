13f0ae6742c203cbb5bf67ba11fe1c785674d2bd
Edisthon <edisthonshuti14@gmail.com>
2026-07-03 22:53:00 +0200

    Add the test method and imports for the convert currency endpoint

--- src/test/java/com/example/currency_converter/CurrencyControllerTest.java
+++ src/test/java/com/example/currency_converter/CurrencyControllerTest.java
@@ -1,10 +1,18 @@
 package com.example.currency_converter;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
-import org.springframework.test.web.servlet.MockMvc;
+import static org.mockito.Mockito.when;
 
 import com.example.currency_converter.service.CurrencyService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
 
 @WebMvcTest
 public class CurrencyControllerTest {
@@ -12,7 +20,23 @@ public class CurrencyControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @MockitoBean
     private CurrencyService currencyService;
+
+    @Test
+    void testConvertCurrencyEndpoint_Success() throws Exception {
+        when(currencyService.convert("USD", "EUR", 100.0)).thenReturn(92.000);
+        mockMvc.perform(get("/convert")
+                .param("from", "USD")
+                .param("to", "EUR")
+                .param("amount", "100.0")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.from").value("USD"))
+                .andExpect(jsonPath("$.to").value("EUR"))
+                .andExpect(jsonPath("$.amount").value(100.0))
+                .andExpect(jsonPath("$.convertedAmount").value(92.0));
+    }
 
 
     
