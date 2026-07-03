6cc833028de68d65c723be762ee941c74f482516
Edisthon <edisthonshuti14@gmail.com>
2026-07-03 15:33:35 +0200

    Adding the fromCurrency, ToCurrency and rate attributes required for the ExchangeRate class

--- src/main/java/com/example/currency_converter/model/ExchangeRate.java
+++ src/main/java/com/example/currency_converter/model/ExchangeRate.java
@@ -1,5 +1,30 @@
 package com.example.currency_converter.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ExchangeRate {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String ID;
+
+    @Column(name = "from_currency", nullable = false, length = 3)
+    private String fromCurrency;
+
+    @Column(name = "to_currency", nullable= false, length = 3)
+    private String toCurrency;
+
+    @Column(nullable = false)
+    private Double rate;
 }
