8d7844470fab217a0ee83e76951f873b7fcf99de
Edisthon <edisthonshuti14@gmail.com>
2026-07-03 15:07:22 +0200

    Add unique id, currency code and currency name as attributes of Currency

--- src/main/java/com/example/currency_converter/model/Currency.java
+++ src/main/java/com/example/currency_converter/model/Currency.java
@@ -1,7 +1,12 @@
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
@@ -9,8 +14,17 @@ import lombok.Data;
 @NoArgsConstructor
 @Table(name = "currency")
 public class Currency {
-
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(length = 3)
+    private String code;
+
+    @Column(nullable = false)
+    private String name;
+
 
     
 }
