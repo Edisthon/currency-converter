04ed11037def2a16c1e3bcabe615287a6d911bf4
Edisthon <edisthonshuti14@gmail.com>
2026-07-03 19:43:47 +0200

    Add instances to allow simulation of db intances

--- src/test/java/com/example/currency_converter/CurrencyServiceTest.java
+++ src/test/java/com/example/currency_converter/CurrencyServiceTest.java
@@ -1,5 +1,24 @@
 package com.example.currency_converter;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import com.example.currency_converter.repository.CurrencyRepository;
+import com.example.currency_converter.repository.ExchangeRateRepository;
+import com.example.currency_converter.service.CurrencyService;
+
 public class CurrencyServiceTest {
-    
+
+    @Mock
+    private CurrencyRepository currencyRepository;
+
+    @Mock
+    private ExchangeRateRepository exchangeRateRepository;
+
+    @InjectMocks
+    private CurrencyService currencyService;
+
 }
