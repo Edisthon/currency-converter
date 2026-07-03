be4e28ad0f06e7e0aa6e9bba6f1ff87207758f6b
Edisthon <edisthonshuti14@gmail.com>
2026-07-03 16:44:46 +0200

    Add the get all currencies endpoint

--- src/main/java/com/example/currency_converter/controller/CurrencyController.java
+++ src/main/java/com/example/currency_converter/controller/CurrencyController.java
@@ -1,9 +1,42 @@
 package com.example.currency_converter.controller;
 
-import org.springframework.stereotype.Controller;
+import java.util.List;
 
-@Controller
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.currency_converter.model.Currency;
+import com.example.currency_converter.service.CurrencyService;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+
+@RestController
+@Slf4j
+@RequestMapping
+@RequiredArgsConstructor
+
 public class CurrencyController{
+
+
+    public final CurrencyService currencyService;
+
+    @GetMapping("/currencies")
+    public ResponseEntity<List<Currency>> getAllCurrencies(){
+
+        log.info("Fetching all currencies");
+        List<Currency> currencies = currencyService.getAllCurrencies();
+
+        return  ResponseEntity.ok(currencies);
+        
+    }
+
+
+    
+
     
 
     
