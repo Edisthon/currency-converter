635e3d28c31fed3f261743dc38fb6283ced0e6c1
Edisthon <edisthonshuti14@gmail.com>
2026-07-03 16:26:43 +0200

    Add the method to do the conversion, and also included the input validations and returning to 3 decimal digit

--- src/main/java/com/example/currency_converter/service/CurrencyService.java
+++ src/main/java/com/example/currency_converter/service/CurrencyService.java
@@ -1,10 +1,13 @@
 package com.example.currency_converter.service;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
 
 import com.example.currency_converter.model.Currency;
+import com.example.currency_converter.model.ExchangeRate;
 import com.example.currency_converter.repository.CurrencyRepository;
 import com.example.currency_converter.repository.ExchangeRateRepository;
 
@@ -24,6 +27,24 @@ public class CurrencyService {
         return currencyRepository.findAll();
     }
 
-    
+    public double convert(String from, String to, double amount){
+        log.info("Converting "+amount+ " "+from+ " to " +to);
+
+        if (amount < 0){
+            throw new IllegalArgumentException("Amount cannot be less thant zero");
+        }
+
+        ExchangeRate exchangeRate = exchangeRateRepository.findByFromCurrencyAndToCurrency(from, to)
+            .orElseThrow(() -> new IllegalArgumentException("Exchange rate not found for "+from+" to "+to));
+
+        
+            double convertedAmount = amount *exchangeRate.getRate();
+
+            BigDecimal bigDecimal = new BigDecimal(Double.toString(convertedAmount));
+            bigDecimal = bigDecimal.setScale(3, RoundingMode.HALF_UP);
+
+            return bigDecimal.doubleValue();
+
+    }
     
 }
