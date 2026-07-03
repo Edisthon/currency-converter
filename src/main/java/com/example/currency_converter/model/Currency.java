0c8a2ee9467bb150f7d6d57b43f772df9d949813
Edisthon <edisthonshuti14@gmail.com>
2026-07-03 15:01:49 +0200

    Initial creation of the model class currency and the required initial annotations

---
+++ src/main/java/com/example/currency_converter/model/Currency.java
@@ -0,0 +1,16 @@
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "currency")
+public class Currency {
+
+    
+
+    
+}
