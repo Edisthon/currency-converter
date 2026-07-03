cc5e83859adf7a552040e4ff326ae19ad707bdf4
Edisthon <edisthonshuti14@gmail.com>
2026-07-03 16:52:18 +0200

    Add the endpoint to convert the currency

--- src/main/java/com/example/currency_converter/controller/CurrencyController.java
+++ src/main/java/com/example/currency_converter/controller/CurrencyController.java
@@ -1,10 +1,12 @@
 package com.example.currency_converter.controller;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.currency_converter.model.Currency;
@@ -35,7 +37,21 @@ public class CurrencyController{
     }
 
 
-    
+    public ResponseEntity<Map<String, Object>> convertCurrency(@RequestParam String from,  @RequestParam String to, @RequestParam double amount){
+
+        log.info("Converting "+amount+""+from+" to " +to);
+
+        double convertedAmount = currencyService.convert(from, to, amount);
+
+        Map<String, Object> response = Map.of(
+            "from", from, 
+            "to", to, 
+            "amount", amount, 
+            "convertedAmount", convertedAmount
+        );
+
+        return ResponseEntity.ok(response);
+    }
 
     
 
