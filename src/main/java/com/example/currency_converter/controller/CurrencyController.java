35b8ad279f10f4fb5b61848f86803d475750ae42
Edisthon <edisthonshuti14@gmail.com>
2026-07-04 17:00:23 +0200

    Add the endpoint to save the currency

--- src/main/java/com/example/currency_converter/controller/CurrencyController.java
+++ src/main/java/com/example/currency_converter/controller/CurrencyController.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -62,6 +64,13 @@ public class CurrencyController{
         log.warn("Invalid input here ", exception.getMessage());
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Map.of("error", "Invalid input", "message", exception.getMessage()));
+    }
+
+    @PostMapping("/currencies")
+    public ResponseEntity<Currency> addCurrency(@RequestBody Currency currency) {
+        log.info("REST Request: Add currency {}", currency.getCode());
+        Currency savedCurrency = currencyService.saveCurrency(currency);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedCurrency);
     }
 
     
